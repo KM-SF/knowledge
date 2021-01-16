16df9cba8d4bf8f87558887418ff7b5255f2d2e0
ysf <594301947@qq.com>
2021-01-16 23:57:07 +0800

    [UPDATE] 更新线程池代码

--- 操作系统/线程/code/thread_pool.c
+++ 操作系统/线程/code/thread_pool.c
@@ -5,29 +5,35 @@
 #include <time.h>
 #include <unistd.h>
 
+#define LL_ADD(item, list)     \
+    do                         \
+    {                          \
+        item->next = list;     \
+        if (list != NULL)      \
+            list->prev = item; \
+        list = item;           \
+    } while (0)
 
-#define LL_ADD(item, list) do {         \
-    item->next = list;                  \
-    if ( list!=NULL ) list->prev = item;  \
-    list = item;                        \
-} while (0)
+#define LL_REMOVE(item, list)              \
+    do                                     \
+    {                                      \
+        if (item->prev != NULL)            \
+            item->prev->next = item->next; \
+        if (item->next != NULL)            \
+            item->next->prev = item->prev; \
+        if (list == item)                  \
+            list = item->next;             \
+        item->prev = item->next = NULL;    \
+    } while (0)
 
-#define LL_REMOVE(item, list) do {      \
-    if ( item->prev != NULL ) item->prev->next = item->next;    \
-    if ( item->next != NULL ) item->next->prev = item->prev;    \
-    if ( list == item ) list = item->next;                      \
-    item->prev = item->next = NULL;                              \
-} while (0)
-
-
-typedef struct NJOB
+typedef struct NTASK
 {
-    void (*job_func)(void *arg);
+    void (*task_func)(void *arg);
     void *arg;
-    struct NJOB *next;
-    struct NJOB *prev;
+    struct NTASK *next;
+    struct NTASK *prev;
 
-}job_t;
+} task_t;
 
 typedef struct NWORKER
 {
@@ -37,85 +43,90 @@ typedef struct NWORKER
     struct NWORKER *next;
     struct NWORKER *prev;
 
-}worker_t;
+} worker_t;
 
 typedef struct NMANAGER
 {
-    job_t *jobs;         // 任务队列的头结点
-    worker_t * workers;  // 执行队列的头结点（线程集合）
+    task_t *tasks;       // 任务队列的头结点
+    worker_t *workers;   // 执行队列的头结点（线程集合）
     int terminate;       // 销毁线程池
     pthread_mutex_t mtx; // 加锁防止出现多个线程抢同一个任务的情况
     pthread_cond_t cond; // 等待任务队列不为空，唤醒线程执行队列
 
-}thread_pool_t;
+} thread_pool_t;
 
+void *nThreadCallback(void *arg)
+{
 
-void *nThreadCallback(void *arg){
-
-    worker_t * worker = (worker_t *)arg;
-    while ( 1 )
+    worker_t *worker = (worker_t *)arg;
+    while (1)
     {
         // if task == NULL -> wait
         pthread_mutex_lock(&worker->pool->mtx);
-        while ( worker->pool->jobs == NULL ) {
+        while (worker->pool->tasks == NULL)
+        {
             // 所有任务都执行完成且有终止标记，退出线程
-            if (worker->pool->terminate) {
-                printf("terminate!!!\n");
+            if (worker->pool->terminate)
+            {
+                printf("thread id:%ld, terminate!!!\n", worker->thread_id);
                 goto end;
             };
-            
+
             pthread_cond_wait(&worker->pool->cond, &worker->pool->mtx);
         }
-        
+
         // get task
         // remove task
-        job_t *job = worker->pool->jobs;
-        if (job != NULL) {
-            LL_REMOVE(job, worker->pool->jobs);
+        task_t *task = worker->pool->tasks;
+        if (task != NULL)
+        {
+            LL_REMOVE(task, worker->pool->tasks);
         }
         pthread_mutex_unlock(&worker->pool->mtx);
         // task->func()
-        if (job != NULL) {
-            job->job_func(job);
-            free(job->arg);
-            free(job);
+        if (task != NULL)
+        {
+            task->task_func(task);
+            free(task->arg);
+            free(task);
         }
     }
 end:
     pthread_mutex_unlock(&worker->pool->mtx);
-
 }
 
-int nThreadPoolPushTask(thread_pool_t *pool, job_t *job){
-
-    // ADD job -> pool->jobs
+int nThreadPoolPushTask(thread_pool_t *pool, task_t *task)
+{
+    // ADD task -> pool->tasks
     pthread_mutex_lock(&pool->mtx);
-    LL_ADD(job, pool->jobs);
+    LL_ADD(task, pool->tasks);
     // notify
-    
-    pthread_mutex_unlock(&pool->mtx);
     pthread_cond_signal(&pool->cond);
-
+    pthread_mutex_unlock(&pool->mtx);
 }
 
-int nThreadPoolCreate(thread_pool_t *pool, int worker_nums) {
-
+int nThreadPoolCreate(thread_pool_t *pool, int worker_nums)
+{
     // check param
-    if ( pool == NULL ) return -1;
-    memset( pool, 0, sizeof(thread_pool_t) );
+    if (pool == NULL)
+        return -1;
+    memset(pool, 0, sizeof(thread_pool_t));
 
-    if (worker_nums < 1 ) worker_nums = 1;
+    if (worker_nums < 1)
+        worker_nums = 1;
 
     // pool -> init
     pthread_mutex_init(&(pool->mtx), NULL);
     pthread_cond_init(&(pool->cond), NULL);
 
     int i = 0;
-    for (i ; i < worker_nums; ++i) {
-        
-        // create worker    
+    for (i; i < worker_nums; ++i)
+    {
+
+        // create worker
         worker_t *worker = (worker_t *)malloc(sizeof(worker_t));
-        if (worker == NULL) {
+        if (worker == NULL)
+        {
             perror("malloc");
             return -2;
         }
@@ -125,25 +136,25 @@ int nThreadPoolCreate(thread_pool_t *pool, int worker_nums) {
 
         // create thread. binb worker <-> thread_id
         int ret = pthread_create(&worker->thread_id, NULL, nThreadCallback, worker);
-        if (ret){
+        if (ret)
+        {
             perror("pthread_create");
             free(worker);
             return -3;
         }
         // add worker -> pool->workers
         LL_ADD(worker, pool->workers);
-    
     }
 
     return 0;
 }
 
-int nThreadPoolDestroy(thread_pool_t *pool) {
-
+int nThreadPoolDestroy(thread_pool_t *pool)
+{
     // 防止多次调用释放资源
     if (pool->terminate)
         return 0;
-    
+
     // 设置线程终止标记
     pool->terminate = 1;
 
@@ -156,15 +167,15 @@ int nThreadPoolDestroy(thread_pool_t *pool) {
     {
         pthread_join(head->thread_id, NULL);
         worker_t *temp = head;
-        head = head->next; 
+        head = head->next;
         LL_REMOVE(temp, pool->workers);
-        if ( temp ){
+        if (temp)
+        {
             free(temp);
             temp = NULL;
         }
-        
     }
-            
+
     pthread_mutex_destroy(&pool->mtx);
     pthread_cond_destroy(&pool->cond);
     free(pool);
@@ -176,32 +187,34 @@ int nThreadPoolDestroy(thread_pool_t *pool) {
 #define WORKER_NUMS 10
 #define COUNTER_SIZE 1000
 
-void jobCbk (void *arg) {
-
-    job_t *job = (job_t *)arg;
-    printf(" counter --> %d\n", *(int *)job->arg);
+void taskCbk(void *arg)
+{
+    task_t *task = (task_t *)arg;
+    printf(" counter --> %d\n", *(int *)task->arg);
 }
 
-int main() {
-
-    thread_pool_t *pool = (thread_pool_t*)malloc(sizeof(thread_pool_t));
+int main()
+{
+    thread_pool_t *pool = (thread_pool_t *)malloc(sizeof(thread_pool_t));
     memset(pool, 0, sizeof(thread_pool_t));
     nThreadPoolCreate(pool, WORKER_NUMS);
-    
+
     int i = 0;
-    for ( i = 0; i < COUNTER_SIZE; i++) {
-        job_t *job = (job_t *)malloc(sizeof(job_t)); 
-        if (job == NULL) {
+    for (i = 0; i < COUNTER_SIZE; i++)
+    {
+        task_t *task = (task_t *)malloc(sizeof(task_t));
+        if (task == NULL)
+        {
             perror("malloc");
             exit(1);
         }
-        memset(job, 0, sizeof(job_t));
+        memset(task, 0, sizeof(task_t));
 
-        job->job_func = jobCbk;
-        job->arg = (int *)malloc(sizeof(int));
-        memset(job->arg, 0, sizeof(int));
-        *( (int *)job->arg ) = i;
-        nThreadPoolPushTask(pool, job);
+        task->task_func = taskCbk;
+        task->arg = (int *)malloc(sizeof(int));
+        memset(task->arg, 0, sizeof(int));
+        *((int *)task->arg) = i;
+        nThreadPoolPushTask(pool, task);
     }
     nThreadPoolDestroy(pool);
 }
